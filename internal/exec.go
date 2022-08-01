702ffe52a60967885aa1f04399175682aedcd4ba
Rob Muhlestein <rob@rwx.gg>
2022-08-01 19:15:03 +0000

    Remove unused internal.SysExec

--- internal/exec.go
+++ internal/exec.go
@@ -8,28 +8,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"syscall"
 )
-
-// SysExec will check for the existence of the first argument as an
-// executable on the system and then execute it using syscall.Exec(),
-// which replaces the currently running program with the new one in all
-// respects (stdin, stdout, stderr, process ID, signal handling, etc).
-// Generally speaking, this is only available on UNIX variations.  This
-// is exceptionally faster and cleaner than calling any of the os/exec
-// variations, but it can make your code far be less compatible
-// with different operating systems.
-func SysExec(args ...string) error {
-	if len(args) == 0 {
-		return fmt.Errorf("missing name of executable")
-	}
-	path, err := exec.LookPath(args[0])
-	if err != nil {
-		return err
-	}
-	// exits the program unless there is an error
-	return syscall.Exec(path, args, os.Environ())
-}
 
 // Exec checks for existence of first argument as an executable on the
 // system and then runs it without exiting in a way that is supported
